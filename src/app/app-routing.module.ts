60c0470058d5edb2267c398456a98d2a01c6254e
Anna Danties <annadanties@gmail.com>
2023-10-24 00:04:52 +0530

    naviagation and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import {ContactComponent} from './pages/contact/contact.component';
+import {ManagebookingComponent} from './pages/managebooking/managebooking.component';
 import {BusListingComponent} from './bus-listing/bus-listing.component';  
+
+
 const routes: Routes = [
-  { path: 'form', component: FormComponent },
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'managebooking',component:ManagebookingComponent},
+  {path: 'form', component: FormComponent },
   {path: 'bus-listing', component: BusListingComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { AppComponent } from './app.component';
 import {BusListingComponent } from '../app/bus-listing/bus-listing.component';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ManagebookingComponent } from './pages/managebooking/managebooking.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
    FormComponent,
-   BusListingComponent
+   BusListingComponent,
+   NavbarComponent,
+   FooterComponent,
+   HomeComponent,
+   ManagebookingComponent,
+   ContactComponent,
+   AboutComponent
   ],
 
   
